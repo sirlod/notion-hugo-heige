a9b776058a4e0b015fbb9d7c41dbd2aa71818a94
Marcin Orzech <marcin.w.orzech@gmail.com>
2024-06-25 12:24:51 +0100

    change url

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,14 +3,14 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://marcinorzech/na-ion-test-370be6e4b1724aacb2e0dc893b40f25e?pvs=4',
+        page_url: 'https://www.notion.so/marcinorzech/Notion-DoIt-3e72c0afcdd14cb3986b29aa0be01b97?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
             //     target_folder: 'path/relative/to/content/folder'
             // }
             {
-                page_id: '45eb121158b9489480ec000fd25c812b',
+                page_id: 'b296b8db44994580941edd16c7ac5356',
                 target_folder: '.'
             }
         ],
@@ -20,7 +20,7 @@ const userConfig: UserConfig = {
             //     target_folder: 'path/relative/to/content/folder'
             // }
             {
-                database_id: 'ad627ada745146a9a7b88d5fcad77d12',
+                database_id: 'b7b1816c05ec464391c8c111fa242985',
                 target_folder: '.'
             }
         ],
